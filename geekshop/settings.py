f93f8e8d520ed14dfa56e44838f0577a84e297a4
SmoktoK <smokotoff@yandex.ru>
2021-12-29 19:47:17 +0300

    Lesson 3. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^nbx*kb8uxd67(*dt^6e^2#wo8a$krs(8dho_+7rlx(vol$44j"
+SECRET_KEY = (
+    "django-insecure-^nbx*kb8uxd67(*dt^6e^2#wo8a$krs(8dho_+7rlx(vol$44j"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
